f7335d5f0cecd9997de268b9e1ea4bf1f5723c68
katyagpe <salas-rodriguez@hotmail.com>
2017-11-09 22:10:52 -0600

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "check-ventas consignacion",
+
+    'summary': """ """,
+
+    'description': """ """,
+
+    'author': "4toAngulo",
+    'website': "",
+
+   
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['sale','product'],
+
+    
+    'data': [
+        'codigo_view.xml',
+    ],
+}
